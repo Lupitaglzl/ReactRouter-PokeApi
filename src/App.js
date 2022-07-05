049d56f9d4367a0e002dea7b9ebfaa86f4184644
Lupitaglz <rociolimon225@gmail.com>
2022-07-04 22:04:44 -0500

    docs:Modifications have been added to the App.js, here we add the import of react-router-dom, as well as the code for the routes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,103 @@
 import React,{useState} from "react";
 import { Usefetch } from "./componentes/Usefetch";
 import { Card } from "./componentes/card/Card";
+import Main from './Components/Main';
 import './App.css';
- 
-const App=()=>{
-  const[url,setUrl]=useState(' https://pokeapi.co/api/v2/pokemon/')
-  const estado =Usefetch(url)
-  const{cargando,data}=estado
-  cargando? console.log('cargando'):console.log(data.results)
-  return (
-      <div>
-          <h1>Lista de Pokemons</h1>
+import './Components/style.css';
+import { Route,Link,BrowserRouter,Routes } from 'react-router-dom';
 
-          {
-              cargando
-              ?
-              <h1>Cargando...</h1>
-              :
-              <div className="caja">
-              <Card results = {data.results} />
 
-                  <div className="container ">
-                      <button onClick={()=>setUrl(data.previous)} className="btn">Anterior</button>
-                      <button onClick={()=>setUrl(data.next)} className="btn">Next</button>
-                  </div>
+const Home = () => <h1>Escoja que opción ver</h1>
+const Pokemons = () => {
+    const[url,setUrl]=useState(' https://pokeapi.co/api/v2/pokemon/')
+    const estado =Usefetch(url)
+    const{cargando,data}=estado
+    cargando? console.log('cargando'):console.log(data.results)
+    return (
+        <div>
+            <h1>Lista de Pokemons</h1>
+            {
+                cargando
+                ?
+                <h1>Cargando...</h1>
+                :
+                <div className="caja">
+                <Card results = {data.results} />
+                    <div className="conten ">
+                        <button onClick={()=>setUrl(data.previous)} className="btn">Anterior</button>
+                        <button onClick={()=>setUrl(data.next)} className="btn">Next</button>
+                    </div>
+                </div>
+            }
+        </div>
+    )
+}
+function Pokemon(){
+    return(
+    <div className="App">
+        <Main />
+    </div>
+)
+} 
 
-              </div>
-          }
-      </div>
-  )
+
+
+const Matricula = () =>{
+    return(
+        <div>
+            <h1 className='matricula'> Mi informacion es la siguiente</h1>
+            <br/>
+            <hr/>
+            <br/>
+            <p className='matricula'>Mi matricula es: 180707</p>
+            <br/>
+            <p className='matricula'>Mi nombre es: Gudalupe del Rocio Gonzalez Limon</p>
+            <br/>
+            <p className='matricula'>Mi carrera es: IDGS </p>
+            <br/>
+            <p className='matricula'>Mi Grupo es: 8-B </p>
+        </div>
+)
 }
 
-export default App;
+
+ 
+
+const App = () => {
+    return(
+      <BrowserRouter>
+        <header>
+            <nav>
+                <Link to='/' className='nav-bar'>
+                    Home
+                </Link>
+                <Link to='/Pokemons' className='nav-bar'>
+                    Pokemons
+                </Link>
+                <Link to='/Pokemon' className='nav-bar'>
+                    Pokemon
+                </Link>
+                <Link to='/Matricula' className='nav-bar'>
+                    Matricula
+                </Link>
+            </nav>
+        </header>
+  
+      <Routes> 
+      <Route exact path='/' element={<Home />} >
+      </Route>
+
+      <Route path='/Pokemons'  element={<Pokemons />}>
+      </Route>
+  
+      <Route path='/Pokemon'  element={<Pokemon />}>
+      </Route>
+  
+      <Route path='/Matricula'  element={<Matricula />}>
+      </Route> 
+      </Routes>
+      </BrowserRouter>
+      )
+  }
+
+  export default App;
